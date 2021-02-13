1ddc0efa62514d97e593597b34bf5dc1f19dbdd1
joiq <frozeefrozee12345@gmail.com>
2021-02-13 14:12:32 +0300

    feat: solve all tasks

--- src/index.js
+++ src/index.js
@@ -1,12 +1,38 @@
 
 exports.min = function min (array) {
-  return 0;
-}
+  if (Array.isArray(array) && array.length) {
+    let m = array[0];
+    array.forEach(elem => {
+      if (elem < m)
+        m = elem;
+    });
+    return m;
+  } else {
+    return 0;
+  }
+};
 
 exports.max = function max (array) {
-  return 0;
-}
+  if (Array.isArray(array) && array.length) {
+    let m = array[0];
+    array.forEach(elem => {
+      if (elem > m)
+        m = elem;
+    });
+    return m;
+  } else {
+    return 0;
+  }
+};
 
 exports.avg = function avg (array) {
-  return 0;
-}
+  if (Array.isArray(array) && array.length) {
+    let sum = 0;
+    array.forEach(elem => {
+      sum += elem;
+    });
+    return sum / array.length;
+  } else {
+    return 0;
+  }
+};
